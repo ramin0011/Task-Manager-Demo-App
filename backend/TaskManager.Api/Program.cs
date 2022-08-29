609bad2930b98c806ce01b47ffe7430b7266e8fe
Ramin <ramin.mh1@hotmail.com>
2022-08-29 15:29:36 +0430

    add jwt to swagger

--- backend/TaskManager.Api/Program.cs
+++ backend/TaskManager.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TaskManager.Api;
 using TaskManager.Application.Services;
 using TaskManager.Application.Services.Interfaces;
@@ -47,7 +48,29 @@ builder.Services.AddScoped(typeof(IUsersService), typeof(UsersService));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
